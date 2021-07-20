b87be0b6fa096812fd80918df91ffa514840220a
Mery Khachikyan <merykhachikian@gmail.com>
2021-07-20 20:49:50 +0400

    factorial of 1-50 in parallel clusters

--- app.js
+++ app.js
@@ -1,21 +1,10 @@
-const express = require('express');
 const cluster = require('cluster');
-const app = express();
-function factorial(num) {
-    let fact = 1;
-    if(num === 0 ){
-        return 1;
+function factorial(n) {
+    if(n>=1) {
+        return factorial(n - 1) * n
     }
-    for (let i = 1; i <= num; i++){
-        fact *= i;
-    }
-    return fact;
+    return 1;
 }
-app.get('/', (req,res) => {
-    const result = factorial(50);
-    console.log(result)
-    res.send(`Ok... ${result}`)
-});
 if(cluster.isMaster){
     for(let i = 0; i < 2; i++){
       cluster.fork();
@@ -23,5 +12,6 @@ if(cluster.isMaster){
     cluster.on('exit', (worker, code, signal) => console.log(`Worker ${worker.process.pid} died`));
 }
 else{
-    app.listen(3000, () => console.log(`🚀 server ${process.pid} @ http://localhost:3000`));
+    console.log(` ${factorial(50)} [${process.pid}]`);
+    process.exit(0);
 }
