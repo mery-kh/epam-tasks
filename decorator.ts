7f45c9c968b4bc768d9ea98b9fb5a0bbb13eda5c
Mery Khachikyan <merykhachikian@gmail.com>
2021-07-25 14:31:31 +0400

    added separate decorators

--- decorator.ts
+++ decorator.ts
@@ -1,73 +1,96 @@
-abstract class Car{
+abstract class Car {
     public description: string;
-    public countOfDoors : number;
-    public power : number;
-    public abstract setColor(color:string): string;
-    public getDescription() : string{
+    public countOfDoors: number;
+    public power: number;
+    public price: number;
+    public abstract cost(): number;
+    public getDescription(): string {
         return this.description;
     };
-    public getPriceFromDoorCount() : number{
-        return this.countOfDoors;
-    };
-    public getPriceFromPower() : number{
-        return this.countOfDoors;
-    };
-    public price: number;
-    public totalPrice:number[];
-    public getPrice(): number{
-        return this.totalPrice.reduce((a,b)=> a+b);
-    };
-    public abstract setEngine(eng:string):string;
-
 }
 abstract class CarOptions extends Car {
     decoratedCar: Car;
-    public abstract getDescription(): string;
 }
 // ------------------------------ Cars ------------------------------
 class BMW extends Car{
     public description = "BMW";
-    public price = 40000;
-    public totalPrice = [this.price];
     public countOfDoors = 2;
     public power = 500;
-    public setEngine(eng: string): string {
-        return eng;
-    }
-    public setColor(color: string): string {
-        return "green";
+    public cost(): number {
+        return 77000;
     }
 }
 // ------------------------------ Options ------------------------------
-class Options extends CarOptions {
+class CarColor extends CarOptions {
+    constructor(car: Car) {
+        super();
+        this.decoratedCar = car;
+        this.price = this.decoratedCar.cost();
+    }
+    public setColor(color:string): string {
 
+        (color === 'red') ? this.price = this.decoratedCar.cost() + 300 : (color === 'black') ? this.price = this.decoratedCar.cost() + 460 : (color === 'silver') ? this.price = this.decoratedCar.cost() + 520 : this.price = this.decoratedCar.cost() + 0
+        return color;
+    }
+    public cost(): number {
+        return this.price;
+    }
+    public getDescription(): string {
+        return `${this.decoratedCar.getDescription()} with color ${this.setColor('red')} decorated`;
+    }
+}
+class CarEngine extends CarOptions {
+    constructor(car: Car) {
+        super();
+        this.decoratedCar = car;
+        this.price = this.decoratedCar.cost();
+    }
+    public setEngine(eng:string):string{
+        (eng === 'Hybrid') ? this.price = this.decoratedCar.cost() + 15000 :  this.price = this.decoratedCar.cost() + 6000;
+        return eng;
+    }
+    public cost(): number {
+        return this.price;
+    }
+    public getDescription(): string {
+        return `${this.decoratedCar.getDescription()} and your car has ${this.setEngine('Oil based')} engine,`;
+    }
+}
+class CarDoors extends CarOptions {
+    constructor(car: Car) {
+        super();
+        this.decoratedCar = car;
+        this.price = this.decoratedCar.cost();
+    }
+    public getDoors(doors:number):number{
+        doors === 4 ? this.price =  this.decoratedCar.cost() + 6100 : this.price =  this.decoratedCar.cost() + 4200;
+        return doors;
+    }
+    public cost(): number {
+        return  this.price;
+    }
+    public getDescription(): string {
+        return `${this.decoratedCar.getDescription()} with ${this.getDoors(4)} doors`;
+    }
+}
+class CarPower extends CarOptions {
     constructor(car: Car) {
         super();
         this.decoratedCar = car;
     }
-    public setEngine(eng:string):string{
-        (eng === 'Hybrid') ? this.decoratedCar.totalPrice.push(15000) : this.decoratedCar.totalPrice.push(6000);
-        return eng;
-    }
-    public getPriceFromDoorCount(): number {
-        return this.countOfDoors === 4 ? this.decoratedCar.totalPrice.push(6100) : this.decoratedCar.totalPrice.push(4200);
-    }
     public getPriceFromPower(): number {
-        return this.power > 600 ? this.decoratedCar.totalPrice.push(710) : this.decoratedCar.totalPrice.push(520);
+        return this.power > 600 ? this.decoratedCar.cost() + 710 : this.decoratedCar.cost() + 520;
     }
-    public setColor(color:string): string {
-        (color === 'red') ? this.decoratedCar.totalPrice.push(300) : (color === 'black') ? this.decoratedCar.totalPrice.push(460) : (color === 'silver') ? this.decoratedCar.totalPrice.push(520) : this.decoratedCar.totalPrice.push(0)
-        return color;
-    }
-    public getPrice(): number {
-        this.getPriceFromDoorCount();
-        this.getPriceFromPower();
-        return this.decoratedCar.totalPrice.reduce((a, b) => a + b);
+    public cost(): number {
+        return this.getPriceFromPower();
     }
     public getDescription(): string {
-        return `${this.decoratedCar.getDescription()} with color ${this.setColor('black')} decorated and your car has ${this.setEngine('Hybrid')} engine and price is ${this.getPrice()}$`;
+        return `${this.decoratedCar.getDescription()} and price is ${this.cost()}$`;
     }
 }
 let bmw = new BMW();
-bmw = new Options(bmw);
+bmw = new CarColor(bmw);
+bmw = new CarEngine(bmw);
+bmw = new CarDoors(bmw);
+bmw = new CarPower(bmw);
 console.log(bmw.getDescription());
