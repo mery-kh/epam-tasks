424626e82261f1db9cb0af288df4f9632c07d6d5
Mery Khachikyan <merykhachikian@gmail.com>
2021-07-30 14:49:58 +0400

    homework7

--- index.js
+++ index.js
@@ -1,23 +1,106 @@
-//node index.js  --folderName files
-const path = require('path');
+const express = require('express')
 const fs = require('fs');
-const zlib = require('zlib');
-const argv = require('minimist')(process.argv.slice(2));
+const app = express();
+const path = require('path');
+const port = 3000;
+const usersJsonPath = path.join(__dirname, 'data', 'users.json');
 
-let oldDir = path.resolve(argv.folderName) ;
-let newDir = path.join(path.resolve(argv.folderName, '..'), 'zipFiles') ;
-if (!fs.existsSync(newDir)){
-    fs.mkdirSync(newDir);
-}
-fs.readdir(oldDir, (err, files) => {
-    if (err)
-        console.log(err);
-    else {
-        files.forEach(file => {
-            let gzip = zlib.createGzip();
-            let r = fs.createReadStream(path.join(__dirname,'files', file));
-            let w = fs.createWriteStream(path.join( newDir,`${file}.gzip`));
-            r.pipe(gzip).pipe(w);
+//middleware
+app.use(express.json());
+
+let users = JSON.parse(fs.readFileSync(usersJsonPath, 'utf-8'));
+
+app.get('/users', (req, res) => {
+    if (users) {
+        res.status(200).json({
+            status: 'success',
+            data: users
         })
     }
+    else {
+        res.status(404).json({
+            success: false,
+            message: 'User doesnt exist'
+        });
+    }
 });
+
+app.get('/users/:id', (req, res) => {
+    let user = users.find(({id}) => id === req.params.id);
+    if (user) {
+        res.status(200).json({
+            status: 'success',
+            data: user
+        })
+    }
+    else {
+        res.status(404).json({
+            success: false,
+            message: 'User doesnt exist'
+        });
+    }
+});
+
+app.post('/users',(req, res) => {
+    const newId = (+users[users.length - 1].id + 1).toString();
+    const newUser = Object.assign({id: newId}, req.body);
+    users.push(newUser);
+    fs.writeFile(usersJsonPath, JSON.stringify(users),
+        err => {
+            res.status(201).json({
+                status: 'success',
+                data: {
+                    user: newUser
+                }
+            });
+        }
+    );
+});
+
+app.put('/users/:id',  (req, res) => {
+        let user = users.find(({id}) => id === req.params.id);
+        if (user) {
+            console.log(user);
+            user.name = req.body.name;
+            console.log(user)
+             fs.writeFile(usersJsonPath,JSON.stringify(users),
+                     err => {
+                 res.status(201).json({
+                     status: 'success',
+                     data: {
+                         user: user
+                     }
+                 });
+             });
+        } else {
+            res.status(400).json({
+                success: false,
+                message: 'User doesnt exist'
+            });
+        }
+});
+app.delete('/users/:id', async (req, res) => {
+    let user = users.find(({id}) => id === req.params.id);
+    if (user) {
+        const index = users.indexOf(user);
+        if (index > -1) {
+            users.splice(index, 1);
+        }
+        fs.writeFile(usersJsonPath, JSON.stringify(users),
+            (err) => {
+                if (err) console.error(err)
+            });
+        res.status(200).json({
+            success: true,
+            data: users[req.params.username],
+            message: 'User deleted'
+        });
+    } else {
+        res.status(404).json({
+            success: false,
+            message: 'User doesnt exist'
+        });
+    }
+});
+
+app.listen(port, () => console.log(`Listening at port ${port}`));
